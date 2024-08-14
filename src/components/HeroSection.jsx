a63d1fe74a8ae2564910a398dfb636a63aa8bf64
ringgavertika <ringgarestu@gmail.com>
2024-08-14 15:56:28 +0700

    responsive

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import React from "react";
-import { Button, Input } from "@nextui-org/react";
+import { Button, Input, Image } from "@nextui-org/react";
 
 const HeroSection = () => {
   const [email, setEmail] = useState("");
@@ -47,31 +47,28 @@ const HeroSection = () => {
   };
 
   return (
-    <div className="flex flex-col md:flex-row p-3 md:p-10">
-      <div className="md:w-1/2 mb-10 md:mb-0">
-        <h2 className="text-4xl md:text-5xl font-bold text-white">Hey Guys! We're coming soon...</h2>
-        <p className="mt-10 text-white">Get ready to be the first to experience the sophistication of the PestDoc-AI app. Register your email to get an invitation when we launch. Thank you.</p>
-
-        <Input type="email" label="Email" value={email} onChange={(e) => setEmail(e.target.value)} client:visible />
-        <Button type="submit" onPress={handleSubmit} className="bg-gradient-to-tr from-[#B8E7BD] to-[#35BA8F] text-white font-medium h-[56px]" style={{ borderRadius: "0.5rem" }} disabled={isSubmitting} client:visible>
-          {isSubmitting ? "Submitting..." : "Notify me"}
-        </Button>
-
-        {responseMessage && <p className="mt-4 text-black">{responseMessage}</p>}
-        <div className="socmed flex space-x-2 mt-10">
+    <div className="flex flex-col md:flex-row p-4 md:p-10 items-center">
+      <div className="w-full md:w-1/2 mb-8 md:mb-0">
+        <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white mb-6">Hey Everybody! We're coming soon...</h2>
+        <p className="text-sm md:text-base text-white mb-8">Get ready to be the first to experience the sophistication of the PestDoc-AI app. Register your email to get an invitation when we launch. Thank you.</p>
+        <form className="flex flex-col sm:flex-row gap-4 mb-6">
+          <Input className="w-full sm:w-2/3" type="email" label="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <Button type="submit" onPress={handleSubmit} className="w-full sm:w-auto bg-gradient-to-tr from-[#B8E7BD] to-[#35BA8F] text-white font-medium h-12" style={{ borderRadius: "0.5rem" }} disabled={isSubmitting}>
+            {isSubmitting ? "Submitting..." : "Notify me"}
+          </Button>
+        </form>
+        {responseMessage && <p className="text-black mb-6">{responseMessage}</p>}
+        <div className="flex space-x-4">
           <a href="https://www.instagram.com/pestdoc_ai" className="text-black">
-            <img src="assets/instagram-landingpage.svg" alt="instagram" className="w-8 h-6" />
+            <img src="assets/instagram-landingpage.svg" alt="instagram" className="w-8 h-8" />
           </a>
           <a href="https://www.tiktok.com/@pestdoc_ai" className="text-black">
-            <img src="assets/tiktok.svg" alt="tiktok" className="w-8 h-6" />
+            <img src="assets/tiktok.svg" alt="tiktok" className="w-8 h-8" />
           </a>
         </div>
       </div>
-      <div className="m-0">
-        <img src="assets/Hp.svg" alt="Hp" />
-      </div>
-      <div>
-        <img src="assets/Human.svg" alt="Human" />
+      <div className="w-full md:w-1/2 mt-8 md:mt-0">
+        <Image src="assets/hp-avatar.svg" alt="Rectangle" className="w-full max-w-md mx-auto" />
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "./node_modules/@nextui-org/theme/dist//*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
